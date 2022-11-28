e107f8dd71e85f576ddb0ebed18d34027acea045
Bianca <biancabatinas64@gmail.com>
2022-11-28 23:45:48 +0200

    New Commit

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ import GameOver from "./screen/GameOver";
 export default function App() {
   const [userNumber, SetUserNumber] = useState();
   const [rounds, SetRounds] = useState(0);
-  
+
 
   const startGame = (selectedNumber) => {
     SetUserNumber(selectedNumber);
@@ -26,11 +26,11 @@ export default function App() {
   //preia numarul ales din start gamews
   //GameScreen preia nr ales din startgame
   let content = <StartGameScreen onStartGame={startGame} />;
-  if (userNumber && rounds <= 0) {
+  if (userNumber && rounds <= 0) { //trecem la GameScreen doar daca avem nr de la utilizator si nr de runde jucate este 0 
     content = (
-      <GameScreen userChoise={userNumber} onGameOver={GameOverHandler} />
+      <GameScreen userChoise={userNumber} onGameOver={GameOverHandler} />// returneaza nr de la utilizator si rundele jucate
     );
-  } else if (rounds > 0) {
+  } else if (rounds > 0) { //daca nr de runde jucate e mai mare ca 0 atunci deja s a jucat
     content = (
       <GameOver
         number={rounds}
